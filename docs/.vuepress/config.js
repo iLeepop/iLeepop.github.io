c8c6b35ea500588bb1269965b3738a77391a15c6
iLeepop <zihao02325@gmail.com>
2023-09-11 22:12:53 +0800

    [config change]

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,23 @@
-import { defineUserConfig } from "vuepress"
+import { defineUserConfig, defaultTheme } from "vuepress"
 
 export default defineUserConfig({
     lang: 'zh-CN',
     title: 'Ilee-Docs',
     description: '欢迎查看我的技术文档',
-    base: '/docs/'
+    base: '/docs/',
+    theme: defaultTheme({
+        navbar: [
+            {
+                text: 'Foo',
+                link: '/foo/'
+            },
+            {
+                text: 'Other',
+                children: ['/other/foo.md', '/other/bar.md']
+            },
+            '/bar/README.md'
+        ],
+        contributorsText: '贡献者',
+        lastUpdatedText: '最后一次更新'
+    })
 })
