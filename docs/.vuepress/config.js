1d30dac1e14601f1c4c089fc57416e4c599904aa
iLeepop <zihao02325@gmail.com>
2023-09-11 21:47:20 +0800

    [base url change]

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,6 @@ import { defineUserConfig } from "vuepress"
 export default defineUserConfig({
     lang: 'zh-CN',
     title: 'Ilee-Docs',
-    description: '欢迎查看我的技术文档'
+    description: '欢迎查看我的技术文档',
+    base: '/docs/'
 })
